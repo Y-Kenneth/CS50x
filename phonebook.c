0379227b15bfb42e7ea32a18d56505234e35a573
Ken <136879307+Y-Kenneth@users.noreply.github.com>
2023-10-21 22:43:57 +0700

    Create phonebook.c

---
+++ phonebook.c
@@ -0,0 +1,18 @@
+// Saves names and numbers to a CSV file
+
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    FILE *file = fopen("phonebook.csv", "a");
+
+    string name = get_string("Name: ");
+    string number = get_string("Number: ");
+
+    // print to a file
+    fprintf(file, "%s,%s\n", name, number);
+
+    fclose(file);
+}
